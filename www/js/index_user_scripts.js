58fcaeb573cb7412795c68f11e7a9f03cba3bcf1
azrin <azrin@absecmy.com>
2017-07-19 06:53:20 +0800

    First Upload
    Upload to git file for build package

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,49 @@
+/*jshint browser:true */
+/*global $ */(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* button  Back */
+    $(document).on("click", ".uib_w_9", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+         return false;
+    });
+    
+        /* button  .uib_w_1 */
+    $(document).on("click", ".uib_w_1", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#info"); 
+         return false;
+    });
+    
+        /* button  .uib_w_2 - azrin create call */
+    $(document).on("click", ".uib_w_2", function(evt)
+    {
+        document.location.href = 'tel:+60380116639';
+        /* your code goes here */ 
+         return false;
+    });
+    
+        /* button  .uib_w_3 */
+    $(document).on("click", ".uib_w_3", function(evt)
+    {
+         /* Other options: .modal("show")  .modal("hide")  .modal("toggle")
+         See full API here: http://getbootstrap.com/javascript/#modals 
+            */
+        
+         $(".uib_w_4").modal("toggle");  
+         return false;
+    });
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
